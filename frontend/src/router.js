abc036c233df0a38f7c0503eb1ac6d50c3bdb45b
kye1115z <kye1115z@naver.com>
2022-07-25 02:57:05 +0900

    feat: OrderConfirmed 컴포넌트 추가

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,6 +4,7 @@ import Registerrr from './Registerrr';
 import Login from './Login';
 import Search from './Search';
 import Custom from './Custom';
+import OrderConfirmed from './OrderConfirmed';
 
 function Router() {
     return (
@@ -14,6 +15,7 @@ function Router() {
                 <Route path='/login' element={<Login />} />
                 <Route path='/search' element={<Search />} />
                 <Route path='/custom' element={<Custom />} />
+                <Route path='/orderconfirmed' element={<OrderConfirmed />} />
                 
             </Routes>
         </BrowserRouter>
