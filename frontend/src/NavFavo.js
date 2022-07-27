d9234ada7275aaa988f998bd7314ecdf63cbd237
kye1115z <kye1115z@naver.com>
2022-07-27 23:24:54 +0900

    feat: NavFavo 컴포넌트 수정

--- frontend/src/NavFavo.js
+++ frontend/src/NavFavo.js
@@ -1,32 +1,63 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import {IoIosArrowBack} from 'react-icons/io'   
-import {BiSearch} from 'react-icons/bi'
-import {BiShoppingBag} from 'react-icons/bi'
 import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
 
 const MypageImg = styled.img`
-    width: 18px;
-    height: 18px;
-    border-radius: 50%;
-    border: none;
-    position: absolute;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  border: none;
+`;
+
+const NavBar = styled.div`
+  width: 100%;
+  height: 27px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`; 
+
+const NavBarBack = styled.div`
+  height: 27px;
+  display: flex;
+  align-items: center;
+  span {
+    font-size: 15px;
+    font-weight: bold;
+  }
+
+  span:first-child  {
+    font-size: 18px;
+    margin-right: 5px;
+  }
+`;
+
+const NavBarBtn = styled.div`
+  width: 90px;
+  height: 27px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 function NavFavo() {
   return (
     <>
-      <Navbar bg="light" variant="light">
-        <Container>
-            <Navbar.Brand href="/register"><IoIosArrowBack className="icon" size="18" color="black" /></Navbar.Brand>
-            <Nav.Link href="/register">Favorites</Nav.Link>
-            <Nav.Link href="/search"><BiSearch className="icon" size="15" color="black" /></Nav.Link>
-            <Nav.Link href="/custom"><BiShoppingBag className="icon" size="15" color="black" /></Nav.Link>
-            <Nav.Link href="/custom"><MypageImg src = "https://post-phinf.pstatic.net/MjAyMTAzMzFfMTMg/MDAxNjE3MTgyNDY5OTQ2.7lToiE1uDhnZ60mKOC8ZdK3xe9PVOx2pNdefS_afqdkg.hiWgkZfjEygAmhUuHcpv61eB80v3pOgS8_3ph8bQ_Ywg.JPEG/%EA%BC%AC%EB%A6%AC%EC%95%BC_%281%29.jpg?type=w1200"></MypageImg></Nav.Link>
+      <NavBar>
+        <Helmet>
+        <link rel="stylesheet" 
+              href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        </Helmet>
 
-        </Container>
-      </Navbar>
+        <NavBarBack>
+          <span class="material-symbols-outlined">arrow_back_ios</span>
+          <span>Favorites</span>
+        </NavBarBack>
+        <NavBarBtn>
+          <span class="material-symbols-outlined">search</span>
+          <span class="material-symbols-outlined">local_mall</span>
+            <MypageImg src = "https://post-phinf.pstatic.net/MjAyMTAzMzFfMTMg/MDAxNjE3MTgyNDY5OTQ2.7lToiE1uDhnZ60mKOC8ZdK3xe9PVOx2pNdefS_afqdkg.hiWgkZfjEygAmhUuHcpv61eB80v3pOgS8_3ph8bQ_Ywg.JPEG/%EA%BC%AC%EB%A6%AC%EC%95%BC_%281%29.jpg?type=w1200" />
+        </NavBarBtn>
+      </NavBar>
     </>
   );
 }
